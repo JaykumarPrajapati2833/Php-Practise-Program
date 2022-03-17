9eddf011f2b76e9a42dd65971f47dad74c4a3d29
jaykumar <jaykumarprajapati2833@gmail.com>
2022-03-17 14:58:20 +0530

    commit

---
+++ Even_Odd.php
@@ -0,0 +1,12 @@
+<?php
+$no=45;
+
+if($no%2==0)
+{
+ echo "Number is even";
+}
+else
+{
+ echo "Number is odd";
+}
+?>
---
+++ Swap_two_number.php
@@ -0,0 +1,12 @@
+<?php
+$no1=45;
+$no2=23;
+$temp;
+
+$temp=$no1;
+$no1=$no2;
+$no2=$temp;
+
+echo "No1 is now: $no1 <br>";
+echo "No2 is now: $no2";
+?>
